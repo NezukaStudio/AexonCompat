487b7c4242de6ee91bd62837a46b12ad93533766
NezukaStidio <nezukastudio.io@gmail.com>
2026-05-08 02:43:19 +0800

    initial commit

---
+++ build/generated/source/buildConfig/release/com/aexon/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.aexon;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.aexon";
+  public static final String BUILD_TYPE = "release";
+}
